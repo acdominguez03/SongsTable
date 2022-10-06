05f3c7abb58ff1920a916976cb1b7f16ddb3133f
acdominguez03 <acordondominguez31@gmail.com>
2022-10-06 20:28:13 +0200

    TableView with cells reorderable

--- SongsTable/SongTableViewController.swift
+++ SongsTable/SongTableViewController.swift
@@ -11,7 +11,7 @@ class SongTableViewController: UITableViewController {
     
     // MARK: - Table view data source
 
-    let songTable = [
+    var songTable = [
         Song(imageUrl: "https://images.genius.com/9048c26badaaec80a2c76eec25f73536.1000x1000x1.jpg", title: "Dancing with the dead", autor: "Powerwolf"),
         Song(imageUrl: "https://m.media-amazon.com/images/I/81JtvRjJkSL._SS500_.jpg", title: "Amaranthine", autor: "Amaranthe"),
         Song(imageUrl: "https://i.scdn.co/image/ab67616d0000b273457163bec7e8e4decf8c6375", title: "Psychosocial", autor: "Slipknot")
@@ -41,6 +41,24 @@ class SongTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 200.0
     }
+    
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .none
+    }
 
+    override func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+    
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let movedObject = self.songTable[sourceIndexPath.row]
+        songTable.remove(at: sourceIndexPath.row)
+        songTable.insert(movedObject, at: destinationIndexPath.row)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.isEditing = true
+    }
 
 }
